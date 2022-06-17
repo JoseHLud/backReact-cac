d767b6b52fc826de082297be9aa8a9b37baa213f
Jose Luduenia <ludjoseh@gmail.com>
2022-06-17 14:31:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors');
+const userModel = require('./model/user.model');
+const middleTest = require('./middleware/index');
+const res = require('express/lib/response');
+const { encrypt, compare, tokenValidator, generateToken } = require('./utils');
+const jwt = require('jsonwebtoken');
+const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+require('./storage/connection');
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Routes
+// app.use('/api/v1/users') routes en api, con version y endpoint
+app.use('/auth', authRoutes);
+app.use('/user', tokenValidator, userRoutes);
+
+
+app.listen(port , ()=> console.log(`> Server is up and runnig on port: ${port}`));
---
+++ middleware/index.js
@@ -0,0 +1,17 @@
+const middleTest = (req , res , next)=>{
+
+const email = req.body.email;
+
+if (!email) {
+    res.status(500).json({
+        message: 'Email is required'
+    });
+    return;
+}
+
+next();
+
+};
+
+
+module.exports = middleTest;
